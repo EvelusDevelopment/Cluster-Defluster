88bc1bed8e290737690d2567f997482b61bfb88d
Hadyn Fitzgerald <sinisoul@gmail.com>
2012-01-09 20:31:40 -0700

    Automated commit.

---
+++ src/org/evelus/Main.java
@@ -0,0 +1,17 @@
+package org.evelus;
+
+/**
+ * Main.java
+ * @version 1.0.0
+ * @author Evelus Development (SiniSoul)
+ */
+public class Main {
+    
+    /**
+     * The main entry point for this program.
+     * @param args The command line arguments. 
+     */
+    public static void main(String[] args) {
+        
+    }
+}
